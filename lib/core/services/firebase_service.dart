5532b054ff62a6a490d0e28a73b56123be03e7a6
mylynxcc <mylynxcc@users.noreply.github.com>
2026-07-27 01:35:32 +0100

    Add FlutterError to foundation.dart import

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -3,7 +3,7 @@ import 'dart:ui' show PlatformDispatcher;
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, FlutterError;
 
 import '../config/env_config.dart';
 
